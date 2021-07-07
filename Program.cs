d101a2be21c0af88267e2dc66c7a855e4200edfb
burak oranlier <oranlier@hotmail.com>
2021-07-07 13:12:50 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace c_sharp_dersleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine ("Hello World:");
+        Console.WriteLine ("İsminizi Giriniz:");
+        string name =Console.ReadLine ();
+        Console.WriteLine ("Soyadınızı Giriniz:");
+        string surname =Console.ReadLine ();
+
+        Console.WriteLine ( "Merhaba, " + name + " " + surname);
+        }
+    }
+}
